b102b5da51e7f475d49e5a8ea81220106c1923bb
Raviraj Mangukiya <ravirajcm@gmail.com>
2016-10-03 01:30:55 -0400

    Added reset functionality on same button

--- DroidApp/app/src/main/java/com/algonquincollege/mang0055/myfirstapp/MainActivity.java
+++ DroidApp/app/src/main/java/com/algonquincollege/mang0055/myfirstapp/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -26,6 +25,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public static String TAG = "MainActivity";
     private static final String ABOUT_DIALOG_TAG = "About Dialog";
     int count = 0;
+    static String btn_text_guess = "Guess";
+    static String btn_text_reset = "Reset";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +34,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         edit_input = (EditText) findViewById(R.id.edit_input);
         btn_guess = (Button) findViewById(R.id.btn_guess);
+        btn_guess.setText(btn_text_guess);
         btn_guess.setOnClickListener(this);
         btn_guess.setOnLongClickListener(this);
         theNumber = generateRandomNumber();
@@ -47,19 +49,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     edit_input.setError("Guess some number first.");
                     return;
                 }
-                try {
-                    int userGuess = Integer.parseInt(edit_input.getText().toString());
-                    if (userGuess == theNumber) {
-                        won(count);
-                    } else if (userGuess < theNumber) {
-                        guessIsLow(count);
-                    } else if (userGuess > theNumber) {
-                        guessIsHigh(count);
+                if (btn_guess.getText().toString().equals(btn_text_guess)) {
+                    try {
+                        int userGuess = Integer.parseInt(edit_input.getText().toString());
+                        if (userGuess == theNumber) {
+                            won(count);
+                        } else if (userGuess < theNumber) {
+                            guessIsLow(count);
+                        } else if (userGuess > theNumber) {
+                            guessIsHigh(count);
+                        }
+                    } catch (NumberFormatException e) {
+                        edit_input.setError("Enter number only. (0 to 1000)");
+                        return;
                     }
-                } catch (NumberFormatException e) {
-                    edit_input.setError("Enter number only. (0 to 1000)");
-                    return;
+                } else if (btn_guess.getText().toString().equals(btn_text_reset)) {
+                    count = 0;
+                    edit_input.setText("");
+                    theNumber = generateRandomNumber();
+                    btn_guess.setText(btn_text_guess);
                 }
+
 
                 break;
         }
@@ -70,7 +80,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (count <= 10) {
             Toast.makeText(getApplicationContext(), "Your guess " + edit_input.getText().toString() + " is high !", Toast.LENGTH_SHORT).show();
         } else {
-            resetGame();
+            resetGameMsg();
         }
     }
 
@@ -79,7 +89,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (count <= 10) {
             Toast.makeText(getApplicationContext(), "Your guess " + edit_input.getText().toString() + " is low !", Toast.LENGTH_SHORT).show();
         } else {
-            resetGame();
+            resetGameMsg();
         }
     }
 
@@ -90,23 +100,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else if (count > 6 && count < 10) {
             msg = "Excellent win!";
         } else {
-            resetGame();
+            resetGameMsg();
             return;
         }
         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+        btn_guess.setText(btn_text_reset);
     }
 
-    private void resetGame() {
-        Toast.makeText(getApplicationContext(), "You are Done with moves.\n\nPlease reset the Game using LongClick Guess.", Toast.LENGTH_SHORT).show();
+    private void resetGameMsg() {
+        btn_guess.setText(btn_text_reset);
+        Toast.makeText(getApplicationContext(), "You are Done with moves.\n\nPlease reset the Game to restart.", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public boolean onLongClick(View view) {
         switch (view.getId()) {
             case R.id.btn_guess:
-                count = 0;
-                edit_input.setText("");
-                theNumber = generateRandomNumber();
+                Toast.makeText(getApplicationContext(), "You're winning guess number is " + theNumber, Toast.LENGTH_LONG).show();
                 break;
         }
         return true;
@@ -115,7 +125,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private int generateRandomNumber() {
         int max = 1000;
         Random r = new Random();
-        int i1 = r.nextInt(max + 1) ;
+        int i1 = r.nextInt(max + 1);
         Log.e(TAG, i1 + " Random Number");
         return i1;
     }
@@ -143,7 +153,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return super.onOptionsItemSelected(item);
     }
 
-    private void showAttempts(){
+    private void showAttempts() {
 
     }
 }
